832e5bedb02a33dec2140ef07de4faab004b4edb
jingyulim <jingyulim>
2023-08-29 11:32:29 +0800

    Converted x subquery into order_clv CTE

--- models/legacy/fct_customer_orders.sql
+++ models/legacy/fct_customer_orders.sql
@@ -30,6 +30,20 @@ with
     where status <> 'fail'
     group by 1
 )
+
+, order_clv as (
+    select
+        p.order_id,
+        sum(t2.total_amount_paid) as clv_bad
+    
+    from paid_orders p
+    left join paid_orders t2 
+        on p.customer_id = t2.customer_id 
+        and p.order_id >= t2.order_id
+    group by 1
+    order by p.order_id
+)
+
 -- final CTE
 -- simple select statement
 
@@ -74,17 +88,6 @@ select
 
 from paid_orders p
 left join customer_orders as c using (customer_id)
-left outer join (
-    select
-        p.order_id,
-        sum(t2.total_amount_paid) as clv_bad
-    
-    from paid_orders p
-    left join paid_orders t2 
-        on p.customer_id = t2.customer_id 
-        and p.order_id >= t2.order_id
-    group by 1
-    order by p.order_id
-) x on x.order_id = p.order_id
+left outer join order_clv x on x.order_id = p.order_id
 
 order by order_id
