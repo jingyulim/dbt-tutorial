e7f74622fcf3ad785ac75d5b90db8591a8e04055
jingyulim <jingyulim>
2023-08-29 11:54:49 +0800

    Removed customer_orders and associated joins

--- models/legacy/fct_customer_orders.sql
+++ models/legacy/fct_customer_orders.sql
@@ -24,7 +24,7 @@ with
 , failed_payments as (
     select 
         orderid as order_id
-        , max(created) as payment_finalized_date, sum(AMOUNT) / 100.0 as total_amount_paid        
+        , max(created) as payment_finalized_date, sum(amount) / 100.0 as total_amount_paid        
 
     from payments
     where status <> 'fail'
@@ -55,18 +55,6 @@ with
     from paid_orders
 )
 
-, customer_orders as (
-    select 
-        customers.id as customer_id
-        , min(order_date) as first_order_date
-        , max(order_date) as most_recent_order_date
-        , count(orders.id) as number_of_orders
-
-    from customers 
-    left join orders on orders.user_id = customers.id 
-    group by 1
-)
-
 -- final CTE
 , final as (
     select
@@ -81,7 +69,6 @@ with
         , customer_orders.first_order_date as fdos
 
     from paid_orders
-    left join customer_orders using (customer_id)
     left outer join order_clv on order_clv.order_id = paid_orders.order_id
 
     order by order_id
