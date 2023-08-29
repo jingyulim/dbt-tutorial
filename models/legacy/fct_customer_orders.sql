608b1f9e4d35f53c0ded3246b086e041ca6e3231
jingyulim <jingyulim>
2023-08-29 11:52:19 +0800

    Updated table aliases

--- models/legacy/fct_customer_orders.sql
+++ models/legacy/fct_customer_orders.sql
@@ -33,18 +33,18 @@ with
 
 , paid_orders as (
     select 
-        Orders.ID as order_id
-        , Orders.USER_ID as customer_id
-        , Orders.ORDER_DATE as order_placed_at
-        , Orders.STATUS as order_status
-        , p.total_amount_paid
-        , p.payment_finalized_date
-        , C.FIRST_NAME as customer_first_name
-        , C.LAST_NAME as customer_last_name
+        orders.id as order_id
+        , orders.user_id as customer_id
+        , orders.order_date as order_placed_at
+        , orders.status as order_status
+        , failed_payments.total_amount_paid
+        , failed_payments.payment_finalized_date
+        , customers.first_name as customer_first_name
+        , customers.last_name as customer_last_name
 
     from orders
-    left join failed_payments p on orders.ID = p.order_id
-    left join customers C on orders.USER_ID = C.ID 
+    left join failed_payments on orders.id = failed_payments.order_id
+    left join customers on orders.user_id = customers.id
 )
 
 , order_clv as (
@@ -52,37 +52,37 @@ with
         order_id
         , sum(total_amount_paid) over(partition by customer_id order by order_id rows between unbounded preceding and current row) as clv_bad
     
-    from paid_orders p
+    from paid_orders
 )
 
 , customer_orders as (
     select 
-        C.ID as customer_id
+        customers.id as customer_id
         , min(ORDER_DATE) as first_order_date
         , max(ORDER_DATE) as most_recent_order_date
         , count(ORDERS.ID) as number_of_orders
 
-    from customers C 
-    left join orders on orders.USER_ID = C.ID 
+    from customers 
+    left join orders on orders.user_id = customers.id 
     group by 1
 )
 
 -- final CTE
 , final as (
     select
-        p.*
-        , row_number() over (order by p.order_id) as transaction_seq
-        , row_number() over (partition by customer_id order by p.order_id) as customer_sales_seq
+        paid_orders.*
+        , row_number() over (order by paid_orders.order_id) as transaction_seq
+        , row_number() over (partition by customer_id order by paid_orders.order_id) as customer_sales_seq
         , case 
-            when c.first_order_date = p.order_placed_at then 'new'
+            when customer_orders.first_order_date = paid_orders.order_placed_at then 'new'
             else 'return' 
           end as nvsr
-        , x.clv_bad as customer_lifetime_value
-        , c.first_order_date as fdos
+        , order_clv.clv_bad as customer_lifetime_value
+        , customer_orders.first_order_date as fdos
 
-    from paid_orders p
-    left join customer_orders as c using (customer_id)
-    left outer join order_clv x on x.order_id = p.order_id
+    from paid_orders
+    left join customer_orders using (customer_id)
+    left outer join order_clv on order_clv.order_id = paid_orders.order_id
 
     order by order_id
 )
